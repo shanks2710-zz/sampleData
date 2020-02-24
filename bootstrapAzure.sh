4c59435030c7d643c083ae686719af324078c4f6
Shashank Sharma <sharma.shas@husky.neu.edu>
2020-02-24 13:04:48 -0500

    Bootstrap Azure

--- bootstrapAzure.sh
+++ bootstrapAzure.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common 
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add - 
+sudo add-apt-repository \
+    "deb https://apt.dockerproject.org/repo/ \
+    ubuntu-$(lsb_release -cs) \
+    main" 
 sudo apt-get update
-sudo apt install docker.io -y 
+sudo apt-get -y install docker-engine 
+# add current user to docker group so there is no need to use sudo when running docker
+sudo usermod -aG docker $(whoami)
 sudo docker pull sharmashas/testapp
 sudo docker run -d -p 8000:80 sharmashas/testapp
